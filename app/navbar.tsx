834554cc6519cb15c60f764b5f14d6d877b3696a
nini1972 <drey@proximus.be>
2025-01-26 15:58:14 +0100

    hide non used pages

--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,12 +1,20 @@
 import Link from 'next/link';
 
 const Navbar = () => {
+  const showAllLinks = false; // Change this to true to show all links
   return (
     <nav className= "bg-yellow-400 text-white p-4">
       <ul className="flex justify-between items-center">
+      {showAllLinks && (
+          <>
         <li className="mx-4">
             <Link href="/">
             Home
+          </Link>
+        </li>
+        <li>
+          <Link href="/blockbuster_chat">
+            Blockbuster Chat 
           </Link>
         </li>
         <li>
@@ -14,14 +22,11 @@ const Navbar = () => {
             Blockbuster Chat V2
           </Link>
         </li>
-        <li>
+       </>
+      )}
+      <li>
           <Link href="/blockbuster_chat_3">
             Blockbuster Chat V3
-          </Link>
-        </li>
-        <li>
-          <Link href="/blockbuster_chat">
-            Blockbuster Chat V1
           </Link>
         </li>
       </ul>

c19e062566d1f220409350461d10c1a2b0fb68bb
nini1972 <drey@proximus.be>
2025-01-26 17:27:03 +0100

    link to instagram

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,17 @@ const WelcomePage: React.FC = () => {
     <div>
       <h1 className='font-bold'>Welcome to my website</h1>
       <p className='mt-4 font-bold'>Click on the Blockbuster Chat V3 link to get started.</p>
+      <p className='mt-4'>
+        Follow me on Instagram: 
+        <a 
+          href="https://www.instagram.com/dominiquereyntjens" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+          className="text-blue-500 hover:underline ml-1"
+        >
+          @dominiquereyntjens
+        </a>
+      </p>
     </div>
   );
 }
